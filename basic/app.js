1440ac5631d4504c1f87899e7a7edbc590f1a076
developeralamgirbd <developer.alamgirbd@gmail.com>
2022-08-10 14:39:03 +0600

    alpine.js tutorial

---
+++ basic/app.js
@@ -0,0 +1,13 @@
+document.addEventListener("alpine:init", ()=>{
+    Alpine.data('dropdown', ()=> ({
+        open: false, 
+        toggle(){
+            this.open = !this.open;
+        }
+    }));
+
+    Alpine.store('currentUser', {
+        username: 'TheCodeholic',
+        posts: ['Post 1', 'Post 2'],
+    })
+})
